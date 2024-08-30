a13aad2ef8c4243e668fe70e25aa852fffe69011
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-31 02:40:37 +0530

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
