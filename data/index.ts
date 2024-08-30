1912e97b55d1eec3a71ee23523c73ca70ffa9341
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-31 01:42:24 +0530

    create portfolio

---
+++ data/index.ts
@@ -0,0 +1,130 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Dedicated to Client Engagement and Transparent Communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an AI Blog Generator",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AI Blogger",
+    des: "Automated blog content creation with GPT-4 and DALL·E3, integrating secure authentication and payment systems, built on Next.js and TypeScript.",
+    img: "/p1.png",
+    link: "https://writer.adityacodes.tech/",
+  },
+  {
+    id: 2,
+    title: "AI Blogger (Landing Page)",
+    des: "Designed an engaging landing page for AI Blogger, leveraging Next.js, Framer Motion, and TailwindCSS to create smooth animations and a visually appealing user experience.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://blogger.adityacodes.tech/",
+  },
+  {
+    id: 3,
+    title: "Multiplayer Chess",
+    des: "Developed a real-time multiplayer chess game with seamless gameplay and scalability, featuring move validation and synchronization using React, WebSockets, and AWS.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://chess.adityacodes.tech/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Engineer",
+    desc: "Developed smart contracts for batch payments, reducing transaction costs by 20%. Leveraged Next.js, TypeScript, and Solidity for performance, and created robust APIs for seamless integration.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Open Source Contributor",
+    desc: "Actively contribute to open source projects, enhancing features and fixing issues",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/aditya-A-G/",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/AdityaGhad47567",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/aditya-ghadge/",
+  },
+];
